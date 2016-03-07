4e56cb70774b4769778dd9c2a324430cd57095c7
Andreas Pålsson <apals@kth.se>
2016-03-07 22:44:05 +0100

    Add link to edit company

--- client/app/company-detail/company-detail.controller.js
+++ client/app/company-detail/company-detail.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('fairManagerApp')
-  .controller('CompanyDetailCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('CompanyDetailCtrl', function ($scope, CompanyService, $routeParams) {
+    CompanyService.Company.get({id: $routeParams.id}, function(response) {
+      $scope.companyName = response.name;
+    }, function(error) {
+      $scope.companyName = "Error fetching data";
+    });
   });
