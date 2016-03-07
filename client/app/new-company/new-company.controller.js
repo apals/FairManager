d29a9f4cdedd450b723631f4c6b607f2ae05ee9e
Andreas Pålsson <apals@kth.se>
2016-03-07 22:08:03 +0100

    Add new-company view

---
+++ client/app/new-company/new-company.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fairManagerApp')
+  .controller('NewCompanyCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/new-company/new-company.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fairManagerApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/companies/new', {
+        templateUrl: 'app/new-company/new-company.html',
+        controller: 'NewCompanyCtrl'
+      });
+  });
