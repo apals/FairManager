9eb4ee761ea3424796c0f533ae816881be64153e
Andreas Pålsson <apals@kth.se>
2016-03-10 16:34:35 +0100

    Set mongolab in production too

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,10 +15,9 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:  process.env.MONGOLAB_URI ||
-          process.env.MONGOHQ_URL ||
-          process.env.OPENSHIFT_MONGODB_DB_URL +
-          process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://localhost/fairmanager'
-  }
+    uri: 'mongodb://admin:password@ds019468.mlab.com:19468/fairmanager-dev'
+  },
+
+  seedDB: true
+
 };
