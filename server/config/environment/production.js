4b1570e0356ad6a08623b0bd277593a632dd64ff
Andreas Pålsson <apals@kth.se>
2016-03-11 13:51:10 +0100

    set development settings as production settings

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,27 +1,15 @@
 'use strict';
 
-// Production specific configuration
-// =================================
+// Development specific configuration
+// ==================================
 module.exports = {
-  // Server IP
-  ip: process.env.OPENSHIFT_NODEJS_IP ||
-  process.env.IP ||
-  undefined,
-
-  // Server port
-  port: process.env.OPENSHIFT_NODEJS_PORT ||
-  process.env.PORT ||
-  8080,
 
   // MongoDB connection options
   mongo: {
-    uri: process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    process.env.OPENSHIFT_MONGODB_DB_URL +
-    process.env.OPENSHIFT_APP_NAME ||
-    'mongodb://admin:password@ds019468.mlab.com:19468/fairmanager-dev'
+    uri: 'mongodb://admin:password@ds019468.mlab.com:19468/fairmanager-dev'
   },
 
+  // Seed database on startup
   seedDB: true
 
 };
