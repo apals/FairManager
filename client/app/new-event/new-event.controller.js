ce5fb173b47d24b23413323c3f3f90cd05120268
Andreas Pålsson <apals@kth.se>
2016-03-15 14:56:57 +0100

    Use form validation

--- client/app/new-event/new-event.controller.js
+++ client/app/new-event/new-event.controller.js
@@ -3,46 +3,28 @@
 angular.module('fairManagerApp')
   .controller('NewEventCtrl', function ($scope, EventService, $location, Upload, $timeout) {
 
-    $scope.addEvent = function () {
+    $scope.addEvent = function (event) {
 
-      console.log("addevent");
-      if ($scope.eventLogo) {
-        $scope.eventLogo.upload = Upload.upload({
+      console.log(event);
+      if (event) {
+        var upload = Upload.upload({
           url: '/api/events',
-          data: {
-            name: $scope.eventName,
-            info: $scope.eventInfo,
-            logo: $scope.eventLogo,
-            startDate: $scope.startDate,
-            endDate: $scope.endDate
-          }
+          data: event
         });
 
-        $scope.eventLogo.upload.then(function (response) {
+        upload.then(function (response) {
           $timeout(function () {
-            $scope.eventLogo.result = response.data;
+            //$scope.eventLogo.result = response.data;
             if (response.status === 201) {
               $location.path('/events');
             }
           });
         }, function (response) {
-          console.log("other one" + response);
           if (response.status > 0)
             $scope.errorMsg = response.status + ': ' + response.data;
         }, function (evt) {
           // Math.min is to fix IE which reports 200% sometimes
-          $scope.eventLogo.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        });
-      } else {
-        var newEvent = new EventService.Events({
-          name: $scope.eventName,
-          info: $scope.eventInfo,
-          startDate: $scope.startDate,
-          endDate: $scope.endDate
-        });
-        newEvent.$save(function (response) {
-          $location.path('/events');
-        }, function (error) {
+          //$scope.eventLogo.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
         });
       }
 
