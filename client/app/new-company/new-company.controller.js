b85ccffac5805a5320526e42ddf59d5938a5f16a
Andreas Pålsson <apals@kth.se>
2016-03-15 15:05:08 +0100

    Add form validation and make code prettier

--- client/app/new-company/new-company.controller.js
+++ client/app/new-company/new-company.controller.js
@@ -3,39 +3,29 @@
 angular.module('fairManagerApp')
   .controller('NewCompanyCtrl', function ($scope, CompanyService, $location, Upload, $timeout) {
 
-    $scope.addCompany = function () {
+    $scope.addCompany = function (company) {
+      console.log("add company");
+      console.log(company);
 
-      if ($scope.companyLogo) {
-        $scope.companyLogo.upload = Upload.upload({
+      if(company) {
+        var upload = Upload.upload({
           url: '/api/companies',
-          data: {
-            name: $scope.companyName,
-            info: $scope.companyInfo,
-            logo: $scope.companyLogo,
-            banner: $scope.companyBanner
-          }
+          data: company
         });
 
-        $scope.companyLogo.upload.then(function (response) {
+        upload.then(function (response) {
           $timeout(function () {
-            $scope.companyLogo.result = response.data;
+            //$scope.eventLogo.result = response.data;
             if (response.status === 201) {
               $location.path('/companies');
             }
           });
         }, function (response) {
-          console.log("other one" + response);
           if (response.status > 0)
             $scope.errorMsg = response.status + ': ' + response.data;
         }, function (evt) {
           // Math.min is to fix IE which reports 200% sometimes
-          $scope.companyLogo.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        });
-      } else {
-        var newCompany = new CompanyService.Companies({name: $scope.companyName, info: $scope.companyInfo});
-        newCompany.$save(function (response) {
-          $location.path('/companies');
-        }, function (error) {
+          //$scope.eventLogo.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
         });
       }
 
