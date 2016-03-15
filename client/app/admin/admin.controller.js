fc7d709ef697a4d207b3b0ad668dbb23a7f3af3d
oalsing <oscar.alsing@gmail.com>
2016-03-15 20:46:20 +0100

    admin can now create new users

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -5,7 +5,19 @@
 class AdminController {
   constructor(User) {
     // Use the User $resource to fetch all users
+    this.user = User;
     this.users = User.query();
+  }
+
+  create(user) {
+  	var newUser = new this.user(user);
+  	console.log("creating user");
+  	var that = this;
+  	newUser.$save(function(response) {
+  		that.users.push(user);
+  	}, function(error) {
+  		this.create.error ="There was an error creating the user";
+  	});
   }
 
   delete(user) {
