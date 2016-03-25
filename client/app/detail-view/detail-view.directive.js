2b6250101f9dc06f78584e2e9717ec9f3ca61937
oalsing <oscar.alsing@gmail.com>
2016-03-25 21:18:38 +0100

    Adds Pretty Detail View

--- client/app/detail-view/detail-view.directive.js
+++ client/app/detail-view/detail-view.directive.js
@@ -7,6 +7,13 @@ angular.module('fairManagerApp')
       scope: {
         element: '='
       },
-      templateUrl: 'app/detail-view/detail-view.html'
+      templateUrl: 'app/detail-view/detail-view.html',
+      link: function($scope) {
+      	console.log($scope.element);
+
+      	$scope.splitCamelCase = function(string) {
+      		return string.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); });
+      	}
+      }
     };
   });
