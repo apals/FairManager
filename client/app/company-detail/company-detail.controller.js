505818f7e95149bb3e3aa8c7d1b08f5b19c64790
oalsing <oscar.alsing@gmail.com>
2016-03-25 21:02:48 +0100

    Fixes loading in detail views

--- client/app/company-detail/company-detail.controller.js
+++ client/app/company-detail/company-detail.controller.js
@@ -3,8 +3,11 @@
 angular.module('fairManagerApp')
   .controller('CompanyDetailCtrl', function ($scope, CompanyService, $routeParams) {
   	$scope.company = {};
+  	$scope.isBusy = true;
+
     CompanyService.Company.get({id: $routeParams.id}, function(response) {
       $scope.company = response;
+      $scope.isBusy = false;
     }, function() {
       $scope.company.error = 'Unable to fetch company details. Please check your internet connection and/or your login credentials.';
     });
--- client/app/personnel-detail/personnel-detail.controller.js
+++ client/app/personnel-detail/personnel-detail.controller.js
@@ -3,9 +3,12 @@
 angular.module('fairManagerApp')
   .controller('PersonnelDetailCtrl', function ($scope, PersonnelService, $routeParams) {
     $scope.personnel = {};
+    $scope.isBusy = true;
 
     PersonnelService.Person.get({id: $routeParams.id}, function(response) {
       $scope.personnel = response;
+      $scope.isBusy = false;
+      
     }, function(error) {
       $scope.personnel.error = 'Unable show personnel. Please check your internet connection and/or your login credentials.';
       console.log(error);
