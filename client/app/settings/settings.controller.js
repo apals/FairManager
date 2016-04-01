767631e19cd0b59d035af387904afe7a644e0f1a
Andreas Pålsson <apals@kth.se>
2016-04-01 22:45:26 +0200

    Add settings service to fetch settings from backend

--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('fairManagerApp')
-  .controller('SettingsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SettingsCtrl', function ($scope, SettingsService) {
+    $scope.settings = SettingsService.Settings.get(function (response) {
+      console.log(response);
+    }, function(error) {
+      console.log(error);
+    });
   });
