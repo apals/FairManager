0430d4cbf22d6674d1000883ed3016b5f49f2ed9
Andreas Pålsson <apals@kth.se>
2016-04-05 13:31:48 +0200

    Add color validator

--- server/api/settings/settings.model.js
+++ server/api/settings/settings.model.js
@@ -3,15 +3,34 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var SettingsSchema = new mongoose.Schema({
-  primaryColor: String,
-  accentColor: String,
-  primaryTextColor: String,
-  primarySubTextColor: String,
-  backgroundColor: String,
+  primaryColor: {
+    type: String,
+    validate: [colorValidator, 'That\'s not a valid color. Input format should be \'#FFFFFFFF\'']
+  },
+  accentColor: {
+    type: String,
+    validate: [colorValidator, 'That\'s not a valid color. Input format should be \'#FFFFFFFF\' a valid color']
+  },
+  primaryTextColor: {
+    type: String,
+    validate: [colorValidator, 'That\'s not a valid color. Input format should be \'#FFFFFFFF\' a valid color']
+  },
+  primarySubTextColor: {
+    type: String,
+    validate: [colorValidator, 'That\'s not a valid color. Input format should be \'#FFFFFFFF\' a valid color']
+  },
+  backgroundColor: {
+    type: String,
+    validate: [colorValidator, 'That\'s not a valid color. Input format should be \'#FFFFFFFF\' a valid color']
+  },
   tabs: [{
     name: String,
     isActive: Boolean
   }]
 });
+
+function colorValidator(v) {
+  return v.indexOf('#') === 0 && v.length === 7;
+}
 
 export default mongoose.model('Settings', SettingsSchema);
