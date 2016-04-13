5f10321906188b745d39c031727af62373d24daf
oalsing <oscar.alsing@gmail.com>
2016-04-13 17:30:35 +0200

    Adds new fields to event

--- server/api/events/events.model.js
+++ server/api/events/events.model.js
@@ -8,6 +8,9 @@ var EventsSchema = new mongoose.Schema({
   location: String,
   startDate: {type: Date, required: true},
   endDate: {type: Date, required: true},
+  registrationRequired: {type: Boolean, required: true},
+  registrationStartDate: {type: Date, required: true},
+  registartionEndDate: {type: Date, required: true},
   imageUrl: String
 });
 
