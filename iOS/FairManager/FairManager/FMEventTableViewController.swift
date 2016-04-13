696559172702b9fa11c879ae6b499c45f8f64936
oalsing <oscar.alsing@gmail.com>
2016-04-13 16:57:35 +0200

    Events tableview sorting and grouping

--- iOS/FairManager/FairManager/FMEventTableViewController.swift
+++ iOS/FairManager/FairManager/FMEventTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class FMEventTableViewController: UITableViewController {
     @IBOutlet weak var refreshCtrl: UIRefreshControl!
     var events:[Event]?
+    var pastFutureEventsDictionary:[String: [Event]]?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,9 +48,36 @@ class FMEventTableViewController: UITableViewController {
             if(events != nil) {
                 self.events = events
                 self.events!.sortInPlace({$0.startDate!.compare($1.startDate!) == NSComparisonResult.OrderedAscending})
-                self.tableView.reloadData()
+                self.pastFutureEventsDictionary = [String: [Event]]()
+                self.pastFutureEventsDictionary!["future"] = []
+                self.pastFutureEventsDictionary!["past"] = []
+                
+                if let events = self.events {
+                    for event in events {
+                        if let startDate = event.startDate {
+                            if startDate.isGreaterThanDate(NSDate()) {
+                                self.pastFutureEventsDictionary!["future"]!.append(event)
+                            } else {
+                                self.pastFutureEventsDictionary!["past"]!.append(event)
+                            }
+                        }
+                    }
+                }
+                
+                self.pastFutureEventsDictionary!["future"]!.sortInPlace({$0.startDate!.compare($1.startDate!) == NSComparisonResult.OrderedAscending})
+                self.pastFutureEventsDictionary!["past"]!.sortInPlace({$0.startDate!.compare($1.startDate!) == NSComparisonResult.OrderedDescending})
+                
+                if(self.tableView.numberOfSections == self.pastFutureEventsDictionary!.keys.map { String($0) }.sort().count) {
+                    let range = NSMakeRange(0, self.tableView.numberOfSections)
+                    let sections = NSIndexSet(indexesInRange: range)
+                    self.tableView.reloadSections(sections, withRowAnimation: .Automatic)
+                } else {
+                    self.tableView.reloadData()
+                }
+                
                 print("sections: \(self.tableView.numberOfSections)")
                 print("rows: \(self.tableView.numberOfRowsInSection(0))")
+                print("rows: \(self.tableView.numberOfRowsInSection(1))")
             }
             
             self.hideLoadingHUD()
@@ -66,19 +95,43 @@ class FMEventTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Future events"
+        } else {
+            return "Past events"
+        }
+    }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        if let dict = self.pastFutureEventsDictionary {
+            return dict.keys.map { String($0) }.sort().count
+        } else {
+            return 0
+        }
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        if let events = self.events {
-            return events.count
-        } else {
-            return 0
+        if let dict = self.pastFutureEventsDictionary {
+            if section == 0 {
+                if let future = dict["future"] {
+                    return future.count
+                }
+            } else {
+                if let past = dict["past"] {
+                    return past.count
+                }
+            }
         }
+        
+        return 0
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -89,30 +142,102 @@ class FMEventTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("FMEventTableViewCell", forIndexPath: indexPath) as! FMEventTableViewCell
         
-        if let events = self.events {
-            let event:Event = events[indexPath.row]
-            print(event)
-            
-            if let name = event.name {
-                cell.eventNameLabel.text = name
+        if let dict = self.pastFutureEventsDictionary {
+            var type:String = ""
+            switch indexPath.section {
+            case 0:
+                type = "future"
+                break
+            case 1:
+                type = "past"
+                break
+            default:
+                return cell
             }
             
-            if let date = event.startDate {
-                let calendar = NSCalendar.currentCalendar()
-                let components = calendar.components([.Day , .Month , .Year], fromDate: date)
+            if let events = dict[type] {
+                let event:Event = events[indexPath.row]
                 
-                let dateFormatter: NSDateFormatter = NSDateFormatter()
-                let months = dateFormatter.shortMonthSymbols
-                let monthSymbol = months[components.month-1]
+                if let name = event.name {
+                    cell.eventNameLabel.text = name
+                }
                 
-                cell.dateTextLabel.text = String(components.day)
-                cell.monthTextLabel.text = monthSymbol
+                if let date = event.startDate {
+                    let calendar = NSCalendar.currentCalendar()
+                    let components = calendar.components([.Day , .Month , .Year], fromDate: date)
+                    
+                    let dateFormatter: NSDateFormatter = NSDateFormatter()
+                    let months = dateFormatter.shortMonthSymbols
+                    let monthSymbol = months[components.month-1]
+                    
+                    cell.dateTextLabel.text = String(components.day)
+                    cell.monthTextLabel.text = monthSymbol
+                }
+
             }
         }
-
         // Configure the cell...
 
         return cell
     }
 
+}
+
+extension NSDate {
+    func isGreaterThanDate(dateToCompare: NSDate) -> Bool {
+        print(self)
+        print(dateToCompare)
+        //Declare Variables
+        var isGreater = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedDescending {
+            isGreater = true
+        }
+        
+        //Return Result
+        return isGreater
+    }
+    
+    func isLessThanDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isLess = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedAscending {
+            isLess = true
+        }
+        
+        //Return Result
+        return isLess
+    }
+    
+    func equalToDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isEqualTo = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedSame {
+            isEqualTo = true
+        }
+        
+        //Return Result
+        return isEqualTo
+    }
+    
+    func addDays(daysToAdd: Int) -> NSDate {
+        let secondsInDays: NSTimeInterval = Double(daysToAdd) * 60 * 60 * 24
+        let dateWithDaysAdded: NSDate = self.dateByAddingTimeInterval(secondsInDays)
+        
+        //Return Result
+        return dateWithDaysAdded
+    }
+    
+    func addHours(hoursToAdd: Int) -> NSDate {
+        let secondsInHours: NSTimeInterval = Double(hoursToAdd) * 60 * 60
+        let dateWithHoursAdded: NSDate = self.dateByAddingTimeInterval(secondsInHours)
+        
+        //Return Result
+        return dateWithHoursAdded
+    }
 }
