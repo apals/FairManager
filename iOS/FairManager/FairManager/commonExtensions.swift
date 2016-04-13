7a40304917a7204a4795b70030d81f0285ab7a10
oalsing <oscar.alsing@gmail.com>
2016-04-13 17:22:18 +0200

    Adds common extensions file

---
+++ iOS/FairManager/FairManager/commonExtensions.swift
@@ -0,0 +1,89 @@
+//
+//  commonExtensions.swift
+//  FairManager
+//
+//  Created by Oscar Alsing on 13/04/16.
+//  Copyright © 2016 Oscar Alsing. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIViewController {
+    func displayErrorOnTableView(view:UITableView){
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, view.bounds.width, view.bounds.height))
+        label.text = "Unable to fetch exhibitors.."
+        label.numberOfLines = 0
+        label.textAlignment = .Center
+        label.sizeToFit()
+        view.backgroundView = label
+        view.separatorStyle = .None
+    }
+    
+    func addBlurView() {
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight] // for supporting device rotation
+        blurEffectView.tag = 666
+        blurEffectView.layer.zPosition = 1
+        view.addSubview(blurEffectView)
+    }
+}
+
+extension NSDate {
+    func isGreaterThanDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isGreater = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedDescending {
+            isGreater = true
+        }
+        
+        //Return Result
+        return isGreater
+    }
+    
+    func isLessThanDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isLess = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedAscending {
+            isLess = true
+        }
+        
+        //Return Result
+        return isLess
+    }
+    
+    func equalToDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isEqualTo = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedSame {
+            isEqualTo = true
+        }
+        
+        //Return Result
+        return isEqualTo
+    }
+    
+    func addDays(daysToAdd: Int) -> NSDate {
+        let secondsInDays: NSTimeInterval = Double(daysToAdd) * 60 * 60 * 24
+        let dateWithDaysAdded: NSDate = self.dateByAddingTimeInterval(secondsInDays)
+        
+        //Return Result
+        return dateWithDaysAdded
+    }
+    
+    func addHours(hoursToAdd: Int) -> NSDate {
+        let secondsInHours: NSTimeInterval = Double(hoursToAdd) * 60 * 60
+        let dateWithHoursAdded: NSDate = self.dateByAddingTimeInterval(secondsInHours)
+        
+        //Return Result
+        return dateWithHoursAdded
+    }
+}
