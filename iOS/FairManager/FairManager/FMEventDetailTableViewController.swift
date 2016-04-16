c8941fe5fb8abd1a53454237c17b79c1606d7d34
oalsing <oscar.alsing@gmail.com>
2016-04-16 15:55:36 +0200

    Adds event detail table view functionality

--- iOS/FairManager/FairManager/FMEventDetailTableViewController.swift
+++ iOS/FairManager/FairManager/FMEventDetailTableViewController.swift
@@ -7,10 +7,17 @@
 //
 
 import UIKit
+import Haneke
 
 class FMEventDetailTableViewController: UITableViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var registrationLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
     
     var event:Event?
+    var registrationLinkIsActive:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +26,8 @@ class FMEventDetailTableViewController: UITableViewController {
         
         showLoadingHUD()
         
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableViewAutomaticDimension
         tableView.tableFooterView = UIView()
     }
     
@@ -28,12 +37,92 @@ class FMEventDetailTableViewController: UITableViewController {
     }
     
     func setEvent(event:Event) {
-        
         self.event = event
         print(event)
+        setupEventView()
         //setupCompanyView()
         removeSubview(666)
         hideLoadingHUD()
+    }
+    
+    private func setupEventView() {
+        banner.clipsToBounds = true
+        if let event = self.event {
+            if let name = event.name {
+                self.navigationItem.title = name
+            }
+            if let url = event.imageUrl {
+                if let url = NSURL(string: url) {
+                    let cache = Shared.imageCache
+                    let fetcher = NetworkFetcher<UIImage>(URL: url)
+                    cache.fetch(fetcher: fetcher).onSuccess { image in
+                        let ratio = image.size.height / image.size.width
+                        
+                        let newHeight = self.banner.frame.width * ratio
+                        
+                        let frame = CGRectMake(0, 0, 320, newHeight)
+                        self.banner.image = image
+                        self.banner.frame = frame
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+            
+            if let registration = event.registrationReguired {
+                if registration {
+                    if let startDate = event.registrationStartDate {
+                        if let endDate = event.registrationEndDate {
+                            let date = NSDate()
+                            if date.isGreaterThanDate(endDate) {
+                                registrationLabel.text = "Registration is over"
+                            } else if date.isLessThanDate(endDate) && date.isGreaterThanDate(startDate){
+                                if let _ = event.registrationUrl {
+                                    registrationLabel.text = "Register here"
+                                    self.registrationLinkIsActive = true
+                                } else {
+                                    registrationLabel.text = "Registration url missing"
+                                }
+                            } else {
+                                registrationLabel.text = "Registration opens at \(startDate.shortDate)"
+                            }
+                        }
+                    }
+                } else {
+                    registrationLabel.text = "No registration required"
+                }
+            } else {
+                registrationLabel.text = "Registration TBA"
+            }
+            
+            if let date = event.startDate {
+                dateLabel.text = tidyEventDateString(date)
+            } else {
+                dateLabel.text = "Date TBA"
+            }
+            
+            if let location = event.location {
+                locationLabel.text = location
+            } else {
+                locationLabel.text = "Location TBA"
+            }
+            
+            if let info = event.info {
+                infoLabel.text = info
+            } else {
+                infoLabel.text = "Information TBA"
+            }
+        }
+    }
+    
+    private func tidyEventDateString(date:NSDate) -> String {
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Day , .Month , .Year, .Hour, .Minute], fromDate: date)
+        
+        let dateFormatter: NSDateFormatter = NSDateFormatter()
+        let months = dateFormatter.shortMonthSymbols
+        let monthSymbol = months[components.month-1]
+        return "\(components.day) \(monthSymbol) \(date.time) \(components.year)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,71 +130,26 @@ class FMEventDetailTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if(indexPath.row == 0 && self.registrationLinkIsActive) {
+            return indexPath
+        } else {
+            return nil
+        }
     }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if(indexPath.row == 0 && self.registrationLinkIsActive) {
+            if let url = event?.registrationUrl{
+                if let url = NSURL(string: url) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
