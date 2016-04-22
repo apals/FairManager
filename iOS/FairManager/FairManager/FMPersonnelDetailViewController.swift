3ebe74f2ecca4c6b5aec440b7a244c5a2af85691
oalsing <oscar.alsing@gmail.com>
2016-04-22 22:39:34 +0200

    adds init FMPersonnelDetailViewController

---
+++ iOS/FairManager/FairManager/FMPersonnelDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FMPersonnelDetailViewController.swift
+//  FairManager
+//
+//  Created by Oscar Alsing on 22/04/16.
+//  Copyright © 2016 Oscar Alsing. All rights reserved.
+//
+
+import UIKit
+
+class FMPersonnelDetailViewController: UIViewController {
+    var person:Person?
+    
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setPerson(person:Person) {
+        self.person = person
+        setupPerson()
+    }
+    
+    private func setupPerson() {
+        if let person = self.person {
+            if let name = person.name {
+                self.navigationItem.title = name
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
