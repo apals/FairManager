3c4589a9626323631ef65a7ab8bd7315a01fd4e5
oalsing <oscar.alsing@gmail.com>
2016-04-23 07:39:01 +0200

    adds ImageViewWithGradient

--- iOS/FairManager/FairManager/ImageViewWithGradient.swift
+++ iOS/FairManager/FairManager/ImageViewWithGradient.swift
@@ -1,9 +1,41 @@
-//
-//  ImageViewWithGradient.swift
-//  FairManager
-//
-//  Created by Oscar Alsing on 23/04/16.
-//  Copyright © 2016 Oscar Alsing. All rights reserved.
-//
+import UIKit
+import AVFoundation
 
-import Foundation
+
+class ImageViewWithGradient: UIImageView
+{
+    let myGradientLayer: CAGradientLayer
+    
+    override init(frame: CGRect)
+    {
+        myGradientLayer = CAGradientLayer()
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        myGradientLayer = CAGradientLayer()
+        super.init(coder: aDecoder)!
+        self.setup()
+    }
+    
+    func setup()
+    {
+        myGradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        myGradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        let colors: [CGColorRef] = [
+            UIColor.clearColor().CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).CGColor
+        ]
+        myGradientLayer.colors = colors
+        myGradientLayer.opaque = false
+        myGradientLayer.locations = [0.0, 0.8]
+        self.layer.addSublayer(myGradientLayer)
+    }
+    
+    override func layoutSubviews()
+    {
+        myGradientLayer.frame = self.layer.bounds
+    }
+}
