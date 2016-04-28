b2f07ebf8742ec7c9b8e09e91b5bdd6c754f3a62
Marcus Heine <mheine@kth.se>
2016-04-28 15:48:09 +0200

    Fix edit-company and minor detail-view change

--- client/app/edit-company/edit-company.controller.js
+++ client/app/edit-company/edit-company.controller.js
@@ -28,11 +28,11 @@ angular.module('fairManagerApp')
     $scope.updateCompany = function (company) {
       var newCompany = {
         name: company.name,
-        logo: company.logo
+        logoUrl: company.logoUrl,
+        logo: company.logo,
+        bannerUrl: company.bannerUrl,
+        banner: company.banner
       };
-      console.log(newCompany);
-      if(company.logoUrl) delete company.logoUrl;
-      if(company.bannerUrl) delete company.bannerUrl;
 
       var upload = Upload.upload({
         method: 'PUT',
@@ -41,14 +41,9 @@ angular.module('fairManagerApp')
       });
 
       upload.then(function (response) {
-        /*$timeout(function () {
-          //$scope.eventLogo.result = response.data;
-          if (response.status === 201) {
-            $location.path('/companies');
-          }
-        });*/
+        $location.path('/companies');
       }, function (error) {
-        //$scope.errorMsg = ErrorHandlingService.getErrorMessage(error, 'create exhibitor');
+        $scope.errorMsg = ErrorHandlingService.getErrorMessage(error, 'create exhibitor');
       }, function () {
         // Math.min is to fix IE which reports 200% sometimes
         //$scope.eventLogo.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
