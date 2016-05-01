fcfa6a876179cfcca3ccbb2c05c35d0e393779e8
Andreas Pålsson <apals@kth.se>
2016-05-01 13:29:43 +0200

    More colors

--- android/app/src/main/java/se/apals/fairmanager/MainActivity.java
+++ android/app/src/main/java/se/apals/fairmanager/MainActivity.java
@@ -16,12 +16,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private Menu menu;
     private SearchView mSearchView;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,13 +69,24 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        SettingsUtils.setActivityColors(this, R.id.toolbar, R.id.tabs);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+        setUpColors();
 
         mViewPager.addOnPageChangeListener(onPageChangeListener);
         mViewPager.setOffscreenPageLimit(3);
 
+    }
+
+    private void setUpColors() {
+        Settings s = SettingsUtils.getSettings(this);
+        SettingsUtils.setActivityColors(this, R.id.toolbar, R.id.tabs);
+        mTabLayout.setSelectedTabIndicatorColor(Color.parseColor(s.getAccentColor()));
+
+        mTabLayout.setTabTextColors(
+            Color.parseColor(s.getAccentColor()),
+            Color.parseColor(s.getAccentColor()));
     }
 
 
@@ -114,15 +122,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //Only create the menu once
         if (this.menu != null) return true;
         this.menu = menu;
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
         MenuItem chatItem = menu.findItem(R.id.action_chat);
-        Drawable[] icons = new Drawable[] {searchItem.getIcon(), chatItem.getIcon()};
+        Drawable[] icons = new Drawable[]{searchItem.getIcon(), chatItem.getIcon()};
 
-        for(Drawable drawable : icons) {
+        for (Drawable drawable : icons) {
             // If we don't mutate the drawable, then all drawable's with this id will have a color
             // filter applied to it.
             drawable.mutate();
@@ -160,40 +169,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
